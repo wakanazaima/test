1cf3e21e2a2db8152248807f55cdd4668faab8ce
wakanazaima <you@example.com>
2021-05-13 16:24:42 +0900

    Q1

---
+++ Q1.rb
@@ -0,0 +1,6 @@
+num1 = 40
+if num1 % 2 == 0
+ puts "num1は偶数です"
+else 
+ puts "num1は奇数です"
+end
---
+++ Q2.rb
@@ -0,0 +1,7 @@
+sum = 0
+count = 1
+while count <= 100 do
+    sum = sum + count
+    count = count + 1
+end
+puts "1〜100までの和を計算すると，#{sum}になります"
